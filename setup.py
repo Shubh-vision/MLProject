b249ef51c6a8c3488004380dfd11db1c0ce0a019
shubham anand <shubhamanand799@gmail.com>
2026-01-26 22:45:50 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirement(file_path:str)-> List[str]:
+    """
+  this function return the list of requirments
+    """
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements        
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="shubh",
+    author_email="shubhamanand799@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirement('requirement.txt')
+
+)
